0b70d31e447dc3a81171a70b896ce611717d493e
shwetakundra <shwetakundra688@gmail.com>
2020-11-24 14:05:15 +0530

    react golf timer

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,51 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.state = { time: 0, x: 0, y: 0,started:false,timeInterval:null};
+    this.keyListener=this.keyListener.bind(this);
+    this.gameStart=this.gameStart.bind(this);
   }
-  componentDidMount() {
+    keyListener(evt){
+    if(this.state.started){
+      if(evt.keyCode===37){
+        this.setState({x: this.state.x-5});
+      }
+      else if(evt.keyCode===38){
+        this.setState({y: this.state.y-5});
+      }
+      else if(evt.keyCode===39){
+        this.setState({x:this.state.x+5});
+      }
+      else if(evt.keyCode===40){
+        this.setState({y:this.state.y+5});
+      }
+    }
+  }
+  gameStart(){
+    this.setState({started:true});
+    this.state.timeInterval=setInterval(()=>{this.setState({time:this.state.time+1})},1*1000);
+    document.addEventListener('keydown',this.keyListener);
+  }
+  componentDidMount(){
+
+  }
+  componentDidUpdate() {
+    if(this.state.started){
+    if(this.state.x===250 && this.state.y===250){
+      clearInterval(this.state.timeInterval)
+      document.removeEventListener('keydown',this.keyListener);
+    }
+  }
     
   }
 
   componentWillUnmount() {
     
   }
-
-
-
   render() {
     return (
  <>
+ {!this.state.started?(<button className="start" onClick={this.gameStart}>start</button>):(<><div className="ball" style={{left:this.state.x+"px",top:this.state.y+"px"}}></div><div className="hole" style={{left:'250px',top:'250px'}}></div><div className="heading-timer">{this.state.time}</div></>)}
 </>
     );
   }
